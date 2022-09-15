0d2018528d4bf91a3ac47c47ccd0987a392805ab
ssheng32 <ssheng32@gatech.edu>
2022-09-15 02:55:53 -0400

    created sign up and login screen

--- mobile/screens/LandingScreen.tsx
+++ mobile/screens/LandingScreen.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export default function LandingScreen(props: any) {
   return (
     <View style={styles.container}>
-      <Text>Landing Screen</Text>
+      <View style={styles.headerContainer}>
+        <View style={styles.logoContainer}></View>
+      </View>
+      <View style={styles.bodyContainer}>
+        <View style={styles.loginContainer}>
+          <Text style={styles.headerText}>Welcome to Healing 4 Heroes!</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={() => props.navigation.navigate("Login")}
+              style={styles.loginBtn}
+            >
+              <Text style={styles.loginBtnText}>Log In</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => props.navigation.navigate("Sign Up")}
+              style={styles.signupBtn}
+            >
+              <Text style={styles.signupBtnText}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,17 +35,76 @@ export default function LandingScreen(props: any) {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#F2F2F2",
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 
-  buttonItem: {
-    width: 300,
-    backgroundColor: "#D3D3D3",
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
+  headerContainer: {
+    flex: 1,
   },
+
+  bodyContainer: {
+    marginTop: 25,
+    flex: 3,
+  },
+
+  logoContainer: {
+    marginTop: 50,
+    padding: 16,
+    borderRadius: 16,
+    alignItems: "center",
+    width: 120,
+    height: 120,
+    backgroundColor: "#666666",
+  },
+
+  loginContainer: {
+    marginTop: 50,
+    padding: 16,
+    borderRadius: 16,
+    alignItems: "center",
+    width: 340,
+    height: 270,
+    backgroundColor: "#fff",
+  },
+
+  headerText: {
+    margin: 10,
+    fontSize: 15,
+    paddingTop: 20,
+    fontWeight: "bold",
+  },
+
+  buttonContainer: {
+    width: "90%",
+    marginTop: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  loginBtn: {
+    marginTop: 20,
+    backgroundColor: "#666666",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+
+  signupBtn: {
+    marginTop: 20,
+    backgroundColor: "#F2F2F2",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+
+  loginBtnText: {
+    color: "white",
+  },
+
+  signupBtnText: {},
 });
--- mobile/screens/SignUpScreen.tsx
+++ mobile/screens/SignUpScreen.tsx
@@ -1,21 +1,186 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
 
-export default function SignUpScreen() {
+export default function SignUpScreen(props: any) {
+  const [checkValidRegister, setCheckValidRegister] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <Text>Sign Up Screen</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <View style={styles.logoContainer}></View>
+      </View>
+
+      <View style={styles.bodyContainer}>
+        <Text style={styles.registerText}>Register your account</Text>
+        <View>
+          <View style={styles.inputContainer}>
+            <Fontisto name="email" size={20} color="grey" />
+            <TextInput placeholder="Email" style={styles.input}></TextInput>
+          </View>
+          <View style={styles.inputContainer}>
+            <SimpleLineIcons name="lock" size={20} color="grey" />
+            <TextInput
+              placeholder="Password"
+              style={styles.input}
+              secureTextEntry
+            ></TextInput>
+          </View>
+          <View style={styles.inputContainer}>
+            <SimpleLineIcons name="lock" size={20} color="grey" />
+            <TextInput
+              placeholder="Confirmed Password"
+              style={styles.input}
+              secureTextEntry
+            ></TextInput>
+          </View>
+        </View>
+
+        {checkValidRegister ? (
+          <View style={styles.failedContainer}>
+            <Text style={styles.failedText}>
+              Register Falied - Account already exists{" "}
+            </Text>
+          </View>
+        ) : (
+          <View></View>
+        )}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => {}} style={styles.button}>
+            <Text style={styles.btnText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.footerContainer}>
+        <View style={styles.changeOptionContainer}>
+          <Text style={styles.footerText}>Already have an account?</Text>
+          <Text
+            style={styles.signupText}
+            onPress={() => {
+              props.navigation.navigate("Login");
+            }}
+          >
+            Sign in here
+          </Text>
+        </View>
+      </View>
+
+      {/* <Text onPress={handleLogin}>Login</Text> */}
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#F2F2F2",
+  },
+
+  headerContainer: {
+    flex: 2,
+    marginTop: 25,
+  },
+
+  bodyContainer: {
+    flex: 3,
+    marginTop: 30,
+    alignItems: "center",
+  },
+
+  footerContainer: {
+    flex: 1,
+    marginTop: 225,
+  },
+
+  logoContainer: {
+    width: 120,
+    height: 120,
+    padding: 16,
+    borderRadius: 16,
+    backgroundColor: "#666666",
+  },
+
+  registerText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginBottom: 15,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    marginTop: 15,
+    paddingVertical: 10,
+    paddingLeft: 10,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    minWidth: "85%",
+    backgroundColor: "white",
+  },
+
+  input: {
+    flex: 1,
+    flexDirection: "row",
+    paddingVertical: 5,
+    paddingLeft: 10,
+    borderRadius: 10,
+    backgroundColor: "white",
+  },
+
+  buttonContainer: {
+    marginTop: 15,
+    alignItems: "center",
+  },
+
+  button: {
+    marginTop: 10,
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+    minWidth: "85%",
+    backgroundColor: "#666666",
+  },
+
+  btnText: {
+    color: "white",
+  },
+
+  failedContainer: {
+    alignItems: "center",
+    marginTop: 25,
+    padding: 8,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    minWidth: "85%",
+    backgroundColor: "#D9D9D9",
+  },
+
+  failedText: {
+    fontSize: 10,
+    fontWeight: "300",
+  },
+
+  changeOptionContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  footerText: {
+    fontWeight: "200",
+  },
+
+  signupText: {
+    fontWeight: "400",
   },
 });
