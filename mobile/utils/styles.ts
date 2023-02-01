82ef859ca08fe6e8e84e94ab8401953886a14632
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-02-01 12:49:37 -0500

    FIxed incorrect syles

--- mobile/utils/styles.ts
+++ mobile/utils/styles.ts
@@ -1,22 +1,15 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  cardShadow: {
-    borderRadius: 16,
-    backgroundColor: "transparent",
+  shadow: {
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 1,
     },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
-  },
-  cardContainer: {
-    backgroundColor: "#fff",
-    borderRadius: 16,
-    overflow: "hidden",
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
   },
 });
 
