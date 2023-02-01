dfc20602863f5f66878a268a8f5fe8ca43d8e1f3
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-02-01 14:30:45 -0500

    Added styles to logo, login and sign up

--- mobile/utils/styles.ts
+++ mobile/utils/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
+const shadowStyle = StyleSheet.create({
   shadow: {
     shadowColor: "#000",
     shadowOffset: {
@@ -13,4 +13,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default styles;
+export default shadowStyle;
