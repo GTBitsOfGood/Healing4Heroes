b860b6df734229e3243a3abea6b7c1395b13a420
David Pang <davidpang314@gmail.com>
2023-02-07 12:31:05 -0500

    revert changes to base URL

--- mobile/utils/urls.ts
+++ mobile/utils/urls.ts
@@ -1,9 +1,9 @@
 function getBaseURL() {
   return `https://healing4heroes-backend.vercel.app`;
-  // if (process.env.NODE_ENV === "production") {
-  //   return `https://healing4heroes-backend.vercel.app`;
-  // }
-  // return "http://localhost:3000";
+  if (process.env.NODE_ENV === "production") {
+    return `https://healing4heroes-backend.vercel.app`;
+  }
+  return "http://localhost:3000";
 }
 
 export const urls = {
