b7a190acaeb6f0fe18919a7b0c5f90dccad2bac0
David Pang <davidpang314@gmail.com>
2023-03-30 07:45:24 -0400

    handle analytics dashboard error

--- mobile/screens/Analytics/AnalyticsDashboardScreen.tsx
+++ mobile/screens/Analytics/AnalyticsDashboardScreen.tsx
@@ -14,7 +14,7 @@ import BaseOverlay from "../../components/Overlays/BaseOverlay";
 import ErrorBox from "../../components/ErrorBox";
 import GenericHeader from "../../components/GenericHeader";
 import { VictoryLegend, VictoryPie } from "victory-native";
-import { ErrorWrapper } from "../../utils/error";
+import { endOfExecutionHandler, ErrorWrapper } from "../../utils/error";
 import { adminGetAnalytics } from "../../actions/Admin";
 
 export default function AnalyticsDashboardScreen(props: any) {
@@ -33,11 +33,15 @@ export default function AnalyticsDashboardScreen(props: any) {
       return true;
     });
     async function loadAnalytics() {
-      const analyticsData = await ErrorWrapper({
-        functionToExecute: adminGetAnalytics,
-        errorHandler: setError,
-      });
-      setAnalytics(analyticsData);
+      try {
+        const analyticsData = await ErrorWrapper({
+          functionToExecute: adminGetAnalytics,
+          errorHandler: setError,
+        });
+        setAnalytics(analyticsData);
+      } catch (error) {
+        endOfExecutionHandler(error as Error);
+      }
     }
     loadAnalytics();
   }, []);
