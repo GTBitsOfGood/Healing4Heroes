33ae869c378282734d1617290eac468cc6fc4c7b
mhartlage3 <madeline.hartlage@gatech.edu>
2023-09-22 04:00:47 -0400

    linting

--- mobile/screens/Onboarding/LandingScreen.tsx
+++ mobile/screens/Onboarding/LandingScreen.tsx
@@ -54,7 +54,7 @@ export default function LandingScreen(props: any) {
             </View>
           ) : null}
           <View style={styles.quoteContainer}>
-            <Text style={styles.quoteText}>"Not all wounds are visible"</Text>
+            <Text style={styles.quoteText}>{`"Not all wounds are visible"`}</Text>
           </View>
         </View>
       }
