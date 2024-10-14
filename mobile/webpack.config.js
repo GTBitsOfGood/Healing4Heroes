f7f8b53d4fd562b6f234e0d5595bb30c474af5e1
Samrat Sahoo <samratsahoo2013@gmail.com>
2024-10-14 12:54:47 -0400

    update webpack

--- mobile/webpack.config.js
+++ mobile/webpack.config.js
@@ -3,5 +3,8 @@ const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   config.resolve.alias["victory-native"] = "victory";
+  config.resolve.fallback = {
+     crypto: false 
+  }
   return config;
 };
